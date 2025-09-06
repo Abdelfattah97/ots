71af96e06d21f2dbefe4893c22ab4b9aa2cedbcc
AbdelfattahMok <abdelfattah.mokheimer@beetleware.com>
2025-09-06 23:35:55 +0300

    allow all origins

---
+++ src/main/java/com/github/ots/common/security/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.github.ots.common.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+    @Configuration
+    public class WebConfig {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**") // all endpoints
+                            .allowedOrigins("http://localhost:4040") // your frontend dev server
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
+    }
+
+
